253122bb60c4b1552115c346612076f60958b4c2
Helen <basargina.helen@gmail.com>
2022-03-25 12:56:21 +0000

    Added tests and libraries for my_kable

--- my_kable.R
+++ my_kable.R
@@ -3,6 +3,20 @@
 #---------------------------------------
 # The result is the pretty Dataset.
 # if you call the function without the second argument, the table will be without a caption.
+#---------------------------------------
+# Libraries
+#---------------------------------------
+if(!require(knitr)){install.packages("knitr")}
+library(knitr)
+#---------------------------------------
+if(!require(kableExtra)){install.packages("kableExtra")}
+library(kableExtra)
+#---------------------------------------
+if(!require(dplyr)){install.packages("dplyr")}
+library(dplyr)
+#---------------------------------------
+# Function
+#---------------------------------------
 my_kable <- function(Dataset, TableCaption=NULL) {
 kable(Dataset
       , col.names = NULL
